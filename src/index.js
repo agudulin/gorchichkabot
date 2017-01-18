922c59ceea555278b3f1fb27c73260c9a765f44b
Alexander Gudulin <alexandr.gudulin@gmail.com>
2017-01-18 18:00:55 +0100

    :zap: use different credentials for prod and dev

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const { getQuote } = require('gorchichka')
 
 const messages = require('./messages')
 
-const app = new Telegraf(process.env.BOT_TOKEN)
+const { NODE_ENV, BOT_TOKEN_PROD, BOT_TOKEN_DEV } = process.env
+const token = NODE_ENV === 'production' ? BOT_TOKEN_PROD : BOT_TOKEN_DEV
+const app = new Telegraf(token)
 
 app.command('start', ctx => ctx.replyWithMarkdown(messages.start))
 app.command('help', ctx => ctx.replyWithMarkdown(messages.help))
