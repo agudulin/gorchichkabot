cfc3d88d06691842ee69d8c52f1722dca377f0f7
Alexander Gudulin <alexandr.gudulin@gmail.com>
2017-01-18 15:07:57 +0100

    :zap: display a quote on command

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const Telegraf = require('telegraf')
+const { getQuote } = require('gorchichka')
 
 const app = new Telegraf(process.env.BOT_TOKEN)
 
 app.command('start', ctx => {
   console.log('start', ctx.from)
-  ctx.reply('welcome')
+  ctx.reply('дратути!\n"так" или "ишо"?')
 })
 
-app.hears('yo', ctx => ctx.reply('yoyo'))
+app.hears('так', ctx => ctx.reply(getQuote()))
+app.hears('ишо', ctx => ctx.reply(getQuote()))
 
 app.startPolling()
