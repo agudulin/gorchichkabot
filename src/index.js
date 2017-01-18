f3748bc982b87f1995f16f2c553a71407cc283e6
Alexander Gudulin <alexandr.gudulin@gmail.com>
2017-01-18 17:40:26 +0100

    :zap: use markdown for messages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const Telegraf = require('telegraf')
 const { getQuote } = require('gorchichka')
 
+const messages = require('./messages')
+
 const app = new Telegraf(process.env.BOT_TOKEN)
 
-app.command('start', ctx => {
-  console.log('start', ctx.from)
-  ctx.reply('дратути!\n"так" или "ишо"?')
-})
+app.command('start', ctx => ctx.replyWithMarkdown(messages.start))
+app.command('help', ctx => ctx.replyWithMarkdown(messages.help))
 
 app.hears(/так/i, ctx => ctx.reply(getQuote()))
 app.hears(/ишо/i, ctx => ctx.reply(getQuote()))
